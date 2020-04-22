67665b6b3fd9ed9a01d12e5130b50ab44ee4eea6
syukur kassim <syukirah@yahoo.co.uk>
2020-04-22 09:30:20 +0800

    added commonly use  getter

--- src/kur-config.service.ts
+++ src/kur-config.service.ts
@@ -12,6 +12,36 @@ export class ConfigService {
     this.envConfig = dotenv.parse(fs.readFileSync('config/' + filePath));
   }
 
+  // Commonly use env var
+  get mongoHost(): string {
+    return this.get('MONGO_HOST');
+  }
+
+  // Commonly use env var
+  get isDevEnv(): boolean {
+    const nodeEnv = this.get('NODE_ENV');
+
+    if (nodeEnv === 'development') {
+      return true;
+    }
+    return false;
+  }
+
+  // Commonly use env var
+  get logSource(): string {
+    return this.get('LOG_SOURCE');
+  }
+
+  // Commonly use env var
+  get appName(): string {
+    return this.get('APP_NAME');
+  }
+
+  // Commonly use env var
+  get microLoggerHost() {
+    return this.get('MICRO_LOGGER_HOST');
+  }
+
   get(key: string): string {
     // check if the key is defined in .env file
     if (this.envConfig[key] === undefined) {
